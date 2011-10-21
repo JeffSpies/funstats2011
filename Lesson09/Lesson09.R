dd5a53f07d58f81a82f79ad359bdef7d3d4992b0
Jeffrey R. Spies <jspies@gmail.com>
2011-10-21 11:43:30 -0400

    Lesson09 done
    Former-commit-id: f4071de995391eedd22368929bb0614552d7b27f

---
+++ Lesson09/Lesson09.R
@@ -0,0 +1,164 @@
+###############################################################################
+# Title: PSYC 7765 - Lesson 09
+# Author: Jeffrey R. Spies (jspies@virginia.edu)
+# History: date()
+#   Fri Oct 21 09:01:32 2011 - Created the file - JRS
+###############################################################################
+
+rm(list=ls()) 
+#setwd("~/Projects/funstats2011/Lesson09")
+require(plyr)
+require(ggplot2)
+
+###############################################################################
+# Read Data
+###############################################################################
+
+dat <- read.csv('bnames.csv', stringsAsFactors=FALSE)
+
+###############################################################################
+# Presidential
+###############################################################################
+
+qplot(year, percent, geom="line", data=dat[dat$name=='Theodore',])
+qplot(year, percent, geom="line", data=dat[dat$name=='Richard',])
+
+###############################################################################
+# Saving images
+###############################################################################
+
+mSelect <- with(dat, (
+            name=='Brandon'
+          | name=='Jordan'
+		| name=='Alexander'
+          | name=='Alex'
+		| name=='David'
+          | name=='Daniel'
+	) & sex=='boy')
+
+fSelect <- with(dat, (	  
+            name=='Bernadette'
+		| name=='Blair'
+		| name=='Kelly'
+		| name=='Karen'
+		| name=='Lucy'
+		| name=='Caitlin'
+		| name=='Erin'
+		| name=='Lindsay'
+		| name=='Marlen'
+		| name=='Rebecca'
+	) & sex=='girl')
+
+tSelect <- FILL IN # Select both males and females
+
+# Three ways of saving your graph
+qplot(year, percent, geom="line", colour=name, data=dat[tSelect,])
+ggsave('plot1.pdf')
+
+p <- qplot(year, percent, geom="point", colour=name, data=subset(dat, tSelect))
+ggsave('plot2.png', plot=p)
+
+p <- qplot(year, percent, geom="smooth", colour=name, data=subset(dat, mSelect))
+pdf(file='plot3.pdf')
+print(p)
+dev.off()
+
+###############################################################################
+# Advanced Selections
+###############################################################################
+
+# What does this do?
+
+c(1,2,3,4,5,6,7,8,9) %in% c(1, 2)
+
+# Apply to the names dataset
+
+mSelect <- with(dat, 
+     # FILL IN
+     & sex=='boy'
+)
+
+unique(dat$name[grep("A", dat$name)])
+unique(dat$name[grep("^A.*a$", dat$name)])
+unique(dat$name[grep(".*(ie|ey)$", dat$name)])
+
+# Plot the last grep above
+qplot(year, percent, data=FILL IN, geom="line", stat="summary", fun.y="sum", colour=sex)
+
+# Do the same with grepl
+qplot(year, percent, data=FILL IN, geom="line", stat="summary", fun.y="sum", colour=sex)
+
+###############################################################################
+# apply, lapply, sapply
+###############################################################################
+
+# What is this?
+apply(iris[,1:4], 2, mean, na.rm=T)
+
+# How about this?
+apply(iris[,1:4], 1, mean, na.rm=T)
+
+# And this? What type of data is this?
+apply(iris[,1:4], c(1,2), mean, na.rm=T)
+
+# How about lapply? sapply?
+
+###############################################################################
+# Proprotion Correct
+###############################################################################
+
+# Fake Dataset: children come in for 2 sessions.  At each session, they are 
+# presented with a spoon with the bowl end facing either their right or left
+# hand 25 times. They either correcly use a radial grip:
+#
+#  o===||||=
+#     \||||
+#
+# or incorrectly us an ulnar grip:
+#
+#     =||||==o
+#     \||||
+
+spoons <- expand.grid(
+     Trial=1:25,
+     Session=1:2,
+     Subject=1:30
+)
+spoons <- spoons[,rev(names(spoons))]
+spoons$isCorrect <- sample(c(T,F),50*30,replace=T)
+
+prop.spoons <- YOU FILL IN # Create a dataset of proportion correct for each subject
+
+prop.spoons.time <- YOU FILL IN # Create a dataset of proportion correct for each subject per each time
+
+###############################################################################
+# Individual subject files
+###############################################################################
+
+# Create folders "Data" and "Graph" in your working directory (getwd())
+
+numberOfSubjects <- 100
+
+l_ply(1:numberOfSubjects, function(i){
+     filename <- paste(paste('Data/Subject', i, sep=""), '.csv', sep="")
+     subjectLevelData <- data.frame(subject=i, trial=1:1000, rt=rnorm(1000, 500, 80)-rnorm(1, mean=0, sd=.1)*(1:1000))
+     write.csv(subjectLevelData, file=filename, row.names=F)
+})
+
+# Plyr Basics    
+#                                 OUTPUT 
+#       INPUT   \ array | data frame |  list | discarded 
+#   ------------|---------------------------------------
+#   array       | aaply |      adply | alply |     a_ply 
+#   data frame  | daply |      ddply | dlply |     d_ply
+#   list        | laply |      ldply | llply |     l_lpy
+
+files <- # Use dir and grep to select just the subjects
+allsubjects <- ldply(files, function(x){
+  read.csv(paste('Data/', x, sep=""))
+}, .progress="text") # Try adding a progress bar
+
+# Use one of the **plys to plot trial x rt for every subject in allsubjects 
+# and put the images in folder "Graphs"
+
+**dply(allsbuejcts, FILLIN)
